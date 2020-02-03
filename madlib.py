0e29229d3c959c812360946148867a4d96f5cc9c
cassady czarnecki <cassady.czarnecki1@marist.edu>
2020-02-03 18:50:07 -0500

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,6 @@
+noun = input ("Enter is a noun.")
+place = input ("Enter is a place.")
+verb = input ("Enter is a verb.")
+adjective = input ("Enter is an adjective.")
+print("I am from " + place +  "where we have" + noun + "which are" + verb + "and are" + adjective)
+
