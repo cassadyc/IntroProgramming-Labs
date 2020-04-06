6dc9b44d49b9cfdaa3741e806fbca9d432dcc1c2
cassadyc <60368667+cassadyc@users.noreply.github.com>
2020-04-06 19:39:21 -0400

    Add files via upload

---
+++ Project1.2.py
@@ -0,0 +1,209 @@
+# CMPT 120 Intro to Programming
+# Project 3 - Game
+# Author: Cassady Czarnecki
+# Created: 2020-04-06
+
+#Global Lists
+locNames = ["Mars","Repair Station","Jupiter","Saturn and Uranus","Earth","Space Station"]
+description = ["It looks like your team has passed Mars!","You have arrived at the repair station and located the parts! Head back to the ship, repair, and get back in the race!","You have passed Jupiter and earned another 100 points!", "Your team has pulled ahead! You just passed Saturn and Uranus!","Press Enter to land on Earth!", "Congratulations! You made it to the space station and have won the race!" ]
+numberMoves = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
+
+hasBeen = ["False", "False", "False", "False", "False", "False"]
+
+score = 'Score:'
+locationscore = "Current location and score"
+moves = 'Moves:'
+
+timeLimit = '12 moves'
+
+def timelimit():
+    while True:
+        if numberMoves == (13):
+            quit()
+            
+def myFunc(food):
+    print(food)
+
+def locale0(description):
+    print(locationscore)
+    print("location:", locNames[0])
+    print(score, 0 +100)
+    print(description[0])
+    print(hasBeen[0])
+    
+def locale1(description):
+    print(locationscore)
+    print("location:", locNames[1])
+    print(score, 0 + 100)
+    print(description[1])
+
+def locale2(description):
+    print(locationscore)
+    print("location:", locNames[2])
+    print(score, 0 + 100 + 100)
+    print(description[2])
+ 
+def locale3(description):
+    print(locationscore)
+    print("location:", locNames[3])
+    print(score, 0 + 100 + 100 + 200)
+    print(description[3])
+
+def locale4(description):
+    print(locationscore)
+    print("location:", locNames[4])
+    print(score, 0 + 100 + 100 +200)
+    print(description[4])
+
+def locale5(description):
+    print(locationscore)
+    print("location:", locNames[5])
+    print(score, 0 + 100 + 100 +200)
+    print(description[5])
+
+
+
+def com():
+    while True:
+        command = input("Please enter command.")
+        if command == "south":
+            print ("You cannot go south.")
+        elif command == "west":
+            print ("You cannot go west.")
+        elif command == "east":
+            print ("You cannot go east.")
+        if command == "north":
+            print ("Continue on")
+            break
+        elif command == "help":
+            he = input("Would you like to quit?")
+            print(he)
+            if he == "yes":
+                print("Game Over")
+                quit()      
+            elif he == "no":
+                print ("Continue on")
+                break
+        elif command == "quit":
+            print ("Game Over")
+            quit()
+        
+def intro():
+    a = ("Welcome to A Space Adventure")
+    print(a)
+    print()
+    introduction = "This game is a race through space!"
+    print(introduction)
+    print()
+    b = ("Today you will be racing past the planets! For every planet you reach you will recieve 100 points. To win you must get 400 points. Your team is 1. To start the race please go to your ship in the basement.")
+    print (b)
+    print()
+    name = input("Enter your team name")
+    print(name)
+    charac = input("Enter a one-word characteristic")
+    print(charac)
+
+def ending(ending):
+    print()
+    print("Copyright Cassady Czarnecki 2020")
+            
+def main():
+    
+    a = intro()
+    print() # Prints a blank line
+    print(score, 0)
+    print(moves, numberMoves[0])
+    c = ("Press Enter to continue...")
+    input(c)
+    print()
+
+    d = ("Congratulations for being a part of the race! We will be starting in 3, 2, 1...GO!")
+    print(d)
+    input(c)
+    print(moves, numberMoves[1])
+    print()
+
+    b = locale0(description)
+    input(c)
+    print(moves, numberMoves[2])
+    print()
+    
+    food = "Cheeseburger"
+    q = ("You may stop for a food break which will be a...")
+    print(q)
+    myFunc(food)
+    input(c)
+    print(moves, numberMoves[3])
+    print()
+    v = ("I hope you enjoyed your break because now its time to get back to the race!")
+    print(v)
+    input(c)
+    print()
+
+    f = ("Oh no! There was a issue with your teams engine. You must land on Jupiter and repair the damage.")
+    print(f)
+    ff = ("Press Enter to inspect the damage.")
+    input(ff)
+    fd = ("Looks like you will need a new part")
+    print(fd)
+    input(c)
+    print(moves, numberMoves[4])
+    print()
+    
+    l = ("Your team must take a detour and find the nearest repair station! The closest one is 0.5 miles north!")
+    print(l)
+    com()
+    input(c)
+    print(moves, numberMoves[5])
+    print()
+    mnl = locale1(description)
+    input(c)
+    print(moves, numberMoves[6])
+    print()
+    
+    g = ("Good job on fixing the issue! Only one team has managed to pass you! Go quick and you might be able to catch up!")
+    print(g)
+    input(c)
+    print(moves, numberMoves[7])
+    mjk = locale2(description)
+    input(c)
+    print(moves, numberMoves[8])
+    print()
+
+    y = ("Keep going you are tied with Team 2!")
+    print(y)
+    mil = locale3(description)
+    print(moves, numberMoves[9])
+    input(c)
+    print("Oh no! You need another part, make a quick trip back to the Reapair Station!")
+    input("Press enter to go back to the Repair Station.")
+    print("You have arrived at the repair station and located the parts! Head back to the ship, repair, and get back in the race!")
+    print("No extra points awarded for going to the same location.")
+    print(moves, numberMoves[10])
+    input(c)
+    print()
+    
+    q = ("The first team to land on Earth and arrive at the space station will win!")
+    print(q)
+    mhk = locale4(description)
+    print(c)
+    print(moves, numberMoves[11])
+    print()
+
+    zz = ("Quick run to the station, Team 2 is right behind you!")
+    print(zz)
+    zx = input("Press the Enter to run!")
+    print(zx)
+    print(moves, numberMoves[12])
+    print()
+    mvk = locale5(description)
+    print()
+
+    ending(ending)
+    
+    
+main()
+
+    
+
+
