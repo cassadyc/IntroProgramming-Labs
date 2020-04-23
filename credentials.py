efad03fe7df8365d2543672c3292e70dfd937f21
cassadyc <60368667+cassadyc@users.noreply.github.com>
2020-04-23 16:50:20 -0400

    A Modular Design

--- credentials.py
+++ credentials.py
@@ -3,26 +3,38 @@
 # Author: Cassady Czarnecki
 # Created: 2020-04-22
 
-def main():
-    # get user's first and last names
-    first = input("Enter your first name: ")
-    last = input("Enter your last name: ")
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+def getname():
+    return (first, last);
+list = getname()
+print(list)
 
-    # TODO modify this to generate a Marist-style username
-    uname = (first + "." + last)
-    print(uname)
 
-    # TODO modify this to ensure the password has at least 8 characters
-    print("Please create a new password.")
-    while True:
-        passwd = input("Create a new password: ")
-        if len(passwd) < 8:
-            print("Fool of a Took! That password is feeble!")
+uname = (first +"."+ last)
+def username():
+        print(uname)
+
+
+def password():
+        print("Please create a new password.")
+        while True:
             passwd = input("Create a new password: ")
-        if len(passwd) > 7:
-            print("The force is strong in this one…")
-            break
-    print("Account configured. Your new email address is", uname + "@marist.edu")
-    
+            if len(passwd) < 8:
+                print("Fool of a Took! That password is feeble!")
+                passwd = input("Create a new password: ")
+            if len(passwd) > 7:
+                print("The force is strong in this one…")
+                break
+        print("Account configured. Your new email address is", uname + "@marist.edumain()") 
+
+
+def main():
+     getname()
+     username()
+     password()
 
 main()
+
+
+        
